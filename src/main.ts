1098a31720977d0fcd242a5e4d59e1ab7145ec14
PaperBackPear3 <fabioangeloni2001@gmail.com>
2024-10-30 18:09:08 +0100

    feat: add skull and rotation

--- src/main.ts
+++ src/main.ts
@@ -1,56 +1,94 @@
-import { FreeCamera } from '@babylonjs/core/Cameras/freeCamera';
+import { AbstractMesh, ActionManager, ArcRotateCamera, ExecuteCodeAction, HemisphericLight, PointerEventTypes, Scene, SceneLoader, Vector3 } from '@babylonjs/core';
 import { Engine } from '@babylonjs/core/Engines/engine';
-import { HemisphericLight } from '@babylonjs/core/Lights/hemisphericLight';
-import { Vector3 } from '@babylonjs/core/Maths/math.vector';
-import { CreateGround } from '@babylonjs/core/Meshes/Builders/groundBuilder';
-import { CreateSphere } from '@babylonjs/core/Meshes/Builders/sphereBuilder';
-import { Scene } from '@babylonjs/core/scene';
 
-import { GridMaterial } from '@babylonjs/materials/grid/gridMaterial';
 
 // Get the canvas element from the DOM.
 const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
 
 // Associate a Babylon Engine to it.
-const engine = new Engine(canvas);
+const engine = new Engine(canvas, true);
 
-// Create our first scene.
-var scene = new Scene(engine);
+const createScene = (): Scene => {
 
-// This creates and positions a free camera (non-mesh)
-var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+  const scene = new Scene(engine);
+  let skull: AbstractMesh;
+  SceneLoader.ImportMeshAsync("", "https://assets.babylonjs.com/meshes/", "skull.babylon", scene).then((result) => {
+    skull = result.meshes[0];
+    skull.position.y = 0;
+    skull.position.x = 0;
+    skull.position.z = 0;
+    skull.actionManager = new ActionManager(scene);
+    skull.actionManager.registerAction(
+      new ExecuteCodeAction(
+      {
+        trigger: ActionManager.OnLeftPickTrigger,
+      },
+      function () {
+        const pickResult = scene.pick(scene.pointerX, scene.pointerY);
+        if (pickResult?.hit) {
+        let isDragging = false;
 
-// This targets the camera to scene origin
-camera.setTarget(Vector3.Zero());
+        scene.onPointerObservable.add((pointerInfo) => {
+          switch (pointerInfo.type) {
+          case PointerEventTypes.POINTERDOWN:
+            isDragging = true;
+            break;
+          case PointerEventTypes.POINTERUP:
+            isDragging = false;
+            break;
+          case PointerEventTypes.POINTERMOVE:
+            if (isDragging) {
+            const deltaX = pointerInfo.event.movementX;
+            const deltaY = pointerInfo.event.movementY;
+            skull.rotate(Vector3.Up(), -deltaX * 0.01);
+            skull.rotate(Vector3.Right(), -deltaY * 0.01);
+            }
+            break;
+          }
+        });
+        }
+      }
+      )
+    );
+  });
 
-// This attaches the camera to the canvas
-camera.attachControl(canvas, true);
+  const camera = new ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 15, new Vector3(7.6, 40, -124), scene);
+  //camera.attachControl(canvas, true);
 
-// This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-var light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
+  camera.onViewMatrixChangedObservable.add(() => {
+    console.log(`Camera position: x=${camera.position.x}, y=${camera.position.y}, z=${camera.position.z}`);
+  });
 
-// Default intensity is 1. Let's dim the light a small amount
-light.intensity = 0.7;
+  new HemisphericLight("light", new Vector3(1, 1, 0), scene);
+  // window.addEventListener('keydown', (event) => {
+  //   if (!skull) return;
 
-// Create a grid material
-var material = new GridMaterial("grid", scene);
+  //   switch (event.key) {
+  //     case 'ArrowUp':
+  //       skull.rotate(Vector3.Right(), Math.PI / 40);
+  //       break;
+  //     case 'ArrowDown':
+  //       skull.rotate(Vector3.Right(), -Math.PI / 40);
+  //       break;
+  //     case 'ArrowLeft':
+  //       skull.rotate(Vector3.Up(), Math.PI / 40);
+  //       break;
+  //     case 'ArrowRight':
+  //       skull.rotate(Vector3.Up(), -Math.PI / 40);
+  //       break;
+  //   }
+  // });
 
-// Our built-in 'sphere' shape.
-var sphere = CreateSphere('sphere1', { segments: 16, diameter: 2 }, scene);
+  return scene;
+};
 
-// Move the sphere upward 1/2 its height
-sphere.position.y = 2;
-
-// Affect a material
-sphere.material = material;
-
-// Our built-in 'ground' shape.
-var ground = CreateGround('ground1', { width: 6, height: 6, subdivisions: 2 }, scene);
-
-// Affect a material
-ground.material = material;
-
+const scene = createScene(); //Call the createScene function
 // Render every frame
 engine.runRenderLoop(() => {
   scene.render();
+});
+
+// Resize the babylon engine when the window is resized
+window.addEventListener('resize', () => {
+  engine.resize();
 });
